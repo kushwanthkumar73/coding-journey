d8cece7690b2cd00fe54815725caf2158f8696d2
kushwanthkumar73 <kushwanth2001@gmail.com>
2025-08-20 15:14:59 +0530

    Day 1 JavaScript essentials -arrays objects functions

---
+++ JavaScript/Day1/employeeDetails.js
@@ -0,0 +1,34 @@
+"use strict";
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", (inputStdin) => {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", (_) => {
+  inputString = inputString
+    .trim()
+    .split("\n")
+    .map((str) => str.trim());
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+function main() {
+  let name = readLine();
+  let role = readLine();
+
+  /* Please do not modify anything above this line */
+
+  let employeeDetailsObject = (name,role)=>({name,role})
+  console.log(employeeDetailsObject(name,role))
+}
