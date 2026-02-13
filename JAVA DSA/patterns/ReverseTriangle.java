6a4b68833254de68c5f0fa9848884d11907cb518
kushwanthkumar73 <kushwanth2001@gmail.com>
2026-02-13 12:30:19 +0530

    Added Nested Loops advanced Problems.

--- JAVA DSA/patterns/ReverseTriangle.java
+++ JAVA DSA/patterns/ReverseTriangle.java
@@ -1,17 +1,13 @@
 import java.util.Scanner;
 
-class InvertedPyramid {
+class ReverseTriangle {
     public static void main(String[] args) {
-        Scanner sc= new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.print("Enter the number of rows: ");
         int n = sc.nextInt();
-        for(int i=1;i<=n;i++){
-            int spaces=i-1;
-            for(int j=1;j<=spaces;j++){
-                System.out.print(" ");
-            }
+        for (int i=1;i<=n;i++){
             int stars = n-i+1;
-            for(int k =1;k<=stars;k++){
+            for(int j=1;j<=stars;j++){
                 System.out.print("*");
             }
             System.out.println();
